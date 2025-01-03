fed7122742e95c939393c0a0fa4a90f8d929703d
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-03 14:01:24 +0200

    some edit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div
             style={{ height: '50px' }}
-            className='flex items-center justify-center text-white fixed-bottom bg-gray-700 mt-7'
+            className='flex items-center justify-center text-white fixed-bottom bg-gray-700 '
         >
             Copyright 2024 Cloud Hosting
         </div>
--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -8,7 +8,7 @@ interface SinglePageParam {
     params: { id: string };
 }
 const SingleArticlePage = async ({ params }: SinglePageParam) => {
-
+    await new Promise(resolve => setTimeout(resolve, 2000));
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
     const article: Article = await response.json();
     if (!response.ok) {
@@ -20,11 +20,15 @@ const SingleArticlePage = async ({ params }: SinglePageParam) => {
             <div className="bg-white p-7 rounded-lg  ">
 
                 <h1 className="text-3xl font-bold text-gray-700 mb-2">{article.title}</h1>
-                <div className="text-gray-300 ">28/12/2024</div>
+                <div className="text-gray-600 text-sm">28/12/2024</div>
                 <p className="text-gray-800 text-xl mt-5">{article.body}</p>
 
             </div>
             <AddCommentForm />
+            <div className="my-5">
+                <h4 className="font-bold ">Comments</h4>
+            </div>
+
             <CommentItem />
             <CommentItem />
 
