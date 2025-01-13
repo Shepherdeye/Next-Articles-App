004190b5c6acfad9eba68f8c99719645692519af
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-13 17:32:52 +0200

    add logout

---
+++ src/app/api/user/logout/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * @method GET
+ * @path api/user/logout
+ * @description Logout user
+ * @access Public
+ */
+
+export function GET(request: NextRequest) {
+    // try {
+    //     const response = NextResponse.json(
+    //         { message: "logout" },
+    //         { status: 200 }
+    //     );
+
+    //     // Remove the cookie by setting it with an expired date
+    //     response.cookies.set('jwtToken', '', {
+    //         path: '/',
+    //         expires: new Date(0), // Set the expiration to a past date
+    //     });
+
+    //     return response;
+    // } catch (error) {
+    //     return NextResponse.json(
+    //         { message: "Failed to log out, internal server error" },
+    //         { status: 500 }
+    //     );
+    // }
+
+    try {
+        const response = NextResponse.json(
+            { message: "Logout Successfully" },
+            { status: 200 }
+        )
+
+        // just  remove the  cookies
+        response.cookies.delete("jwtToken");
+        return response
+    } catch (error) {
+
+    }
+
+}
--- src/utils/verfyJwt.ts
+++ src/utils/verfyJwt.ts
@@ -3,19 +3,39 @@ import { NextRequest } from 'next/server';
 import { JwtType } from './types';
 
 // Verify Token For API End Point
+// export function verifyToken(request: NextRequest): JwtType | null {
+//     try {
+//         const authToken = request.cookies.get("jwtToken");
+//         const jwtToken = authToken?.value as string;
+//         if (!jwtToken) return null;
+
+//         const privateKey = process.env.SECRET_KEY as string;
+//         const userPayload = jwt.verify(jwtToken, privateKey) as JwtType;
+
+//         return userPayload;
+//     } catch (error) {
+//         return null;
+//     }
+// }
+
 export function verifyToken(request: NextRequest): JwtType | null {
     try {
-        const authToken = request.cookies.get("jwtToken");
-        const jwtToken = authToken?.value as string;
-        if (!jwtToken) return null;
 
-        const privateKey = process.env.SECRET_KEY as string;
-        const userPayload = jwt.verify(jwtToken, privateKey) as JwtType;
 
-        return userPayload;
+        const jwtTokenPath = request.cookies.get("jwtToken");
+
+        const token = jwtTokenPath?.value as string;
+
+        if (!token) return null;
+
+        const secretKey = process.env.SECRET_KEY as string;
+
+        const userPaylodFromToken = jwt.verify(token, secretKey) as JwtType;
+
+        return userPaylodFromToken;
+
     } catch (error) {
-        return null;
+        return null
     }
+
 }
-
-
