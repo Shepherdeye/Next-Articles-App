9ab2a2d495ac57865082be39215c6e9d017c2414
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-15 03:08:05 +0200

    font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 
-// import { Noto_Kufi_Arabic } from "next/font/google";
+import { Noto_Kufi_Arabic } from "next/font/google";
 import NavBar from "@/Components/Navbar/Navbar";
 import "./globals.css";
 
 import Footer from "@/components/Footer/Footer";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, Zoom } from "react-toastify";
-// const noto_Kufi_Arabic = Noto_Kufi_Arabic({ subsets: ["latin"], weight: "400" });
+const noto_Kufi_Arabic = Noto_Kufi_Arabic({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Cloud App",
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         style={{
           background: "#efefef"
         }}
-      // className={noto_Kufi_Arabic.className}
+        className={noto_Kufi_Arabic.className}
       >
         <NavBar />
         <ToastContainer transition={Zoom} position="top-center" theme="colored" />
