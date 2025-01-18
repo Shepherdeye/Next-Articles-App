fabb0166133e83cee295b4240e5d443d3eec7d14
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-18 16:05:17 +0200

    continue search

--- src/app/api/articles/search/route.ts
+++ src/app/api/articles/search/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/utils/db";
 import { NextRequest, NextResponse } from "next/server"
 
 
@@ -12,10 +13,30 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
     try {
+        const searchText = request.nextUrl.searchParams.get("searchText");
+        let articles;
+        if (searchText) {
+            articles = await prisma.article.findMany(
+                {
+                    where: {
+                        title: {
+                            startsWith: searchText,
+                            mode: "insensitive"
+                        }
+                    }
+                }
+            )
+            return NextResponse.json(
+                articles,
+                { status: 200 }
+            )
+        }
+
+
 
     } catch (error) {
         return NextResponse.json(
-            { message: "Internal Server Error" }
+            { message: "Internal Server Errorw" }
         )
 
     }
