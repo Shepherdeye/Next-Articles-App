644422f60a8ea542ee9cf64f3cdb738c8674be3d
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-21 03:33:40 +0200

    add  logout

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import "./Navbar.css"
+import { FaRegCircleUser } from "react-icons/fa6";
+
 import Headnav from "./Headnav";
 import Link from 'next/link'
 
 import { verifyTokenForPages } from "@/utils/verfyJwt";
 import getToken from "@/getToken/getToken";
+import LogoutButton from "./LogoutButton";
 
 
 async function NavBar() {
@@ -19,8 +22,15 @@ async function NavBar() {
                 <div className="resgestration_item_wrapper">
                     {
                         user ? (<>
-
-                            {user.name}
+                            <div className="mx-2 flex justify-content-end items-center">
+                                <FaRegCircleUser size={30} />
+                                <Link href="/" >
+                                    <span style={{ textTransform: "capitalize" }} className='text-blue-700 font-bold md:text-xl mx-2'>
+                                        {user?.name}
+                                    </span>
+                                </Link>
+                            </div>
+                            <LogoutButton />
 
 
                         </>) : (<>
