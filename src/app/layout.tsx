c4e02cd35961011abf2ff3c15acc164f4f17d333
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-29 09:34:59 +0200

    change  domain and  the  font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import { Cairo } from "next/font/google";
+// import { Cairo } from "next/font/google";
 
 import NavBar from "@/Components/Navbar/Navbar";
 import "./globals.css";
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, Zoom } from "react-toastify";
 import Footer from "@/Components/Footer/Footer";
 
-const poppinsFont = Cairo({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
+// const poppinsFont = Cairo({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export const metadata: Metadata = {
   title: "Cloud App",
@@ -23,7 +23,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en"
-      className={poppinsFont.className}
+    // className={poppinsFont.className}
     >
       <body
         style={{
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,10 @@
 export const ARTICLE_PER_PAGE = 6;
-export const DOMAIN = "https://next-articles-app.vercel.app"
+
+const PRODUCTION_DOMAIN = "https://next-articles-app.vercel.app";
+
+const DEVELOPMENT_DOMAIN = "http://localhost:3000/";
+
+export const DOMAIN = process.env.NODE_ENV == "production"
+    ? PRODUCTION_DOMAIN :
+    DEVELOPMENT_DOMAIN;
+
